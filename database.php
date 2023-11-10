c7da44721fbc4b863898c7d6a70dd677783ca51c
Michał Giza <michalgiza8b@gmail.com>
2023-11-10 15:33:40 +0100

    connection w with database

---
+++ database.php
@@ -0,0 +1,15 @@
+<?php
+
+    $config = array('host' => 'localhost', 'user' => 'root', 'password' => '', 'database' => 'gwork');
+
+    try
+    {
+        $db = new PDO("mysql:host={$config['host']}; dbname={$config['database']}; charset=utf8", $config['user'], $config['password']);
+        $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false); //zabezpieczenie przed SQL injection
+        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }
+    catch(PDOException $e)
+    {
+        echo "Błąd bazy danych: ".$e->getMessage();
+    }
+?>
